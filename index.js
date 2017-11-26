fc3675dd65e89af85eea5c43860a9cd93ae7a3a9
Steven Taylor <steven.taylor@me.com>
2017-11-26 14:48:55 -0800

    Cleanup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+module.exports.db = require('./lib/db.js');
 module.exports.mongo = require('./lib/mongo.js');
 module.exports.mysql = require('./lib/mysql.js');
 module.exports.securityHandlers = require('./lib/securityHandlers.js');
