7202713f951999253af45c5f429397a26f9bfe73
Steven Taylor <steven.taylor@me.com>
2017-12-26 19:37:02 -0800

    update

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ module.exports.db = require('./lib/db.js');
 module.exports.mongo = require('./lib/mongo.js');
 module.exports.mysql = require('./lib/mysql.js');
 module.exports.securityHandlers = require('./lib/securityHandlers.js');
+module.exports.auth = require('./lib/auth.js');
