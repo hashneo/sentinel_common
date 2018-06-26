5a47d766dd31ca9988a7eef7187f8bc88fe8702c
Steven Taylor <steven.taylor@me.com>
2018-06-26 14:20:25 -0700

    updated

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ module.exports.mongo = require('./lib/mongo.js');
 module.exports.mysql = require('./lib/mysql.js');
 module.exports.securityHandlers = require('./lib/securityHandlers.js');
 module.exports.auth = require('./lib/auth.js');
+module.exports.logger = require('./lib/logger.js');
